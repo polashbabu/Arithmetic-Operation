f9b46630a373fb5216ab434ed7ac4bb6c292e17d
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-02-25 11:10:24 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArithmaticOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number");
+            int numbe1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int number2 = int.Parse(Console.ReadLine());
+            int Add = numbe1 + number2;
+            int sub = numbe1 - number2;
+            int mul = numbe1 * number2;
+            float div = (float)numbe1 / number2;
+            int mod = numbe1 % number2;
+            Console.WriteLine("Sum of two number is :{0}",Add);
+            Console.WriteLine("Subtraction of two number is :{0}",sub);
+            Console.WriteLine("multiple of two number is :{0}",mul);
+            Console.WriteLine("Division of two number is :{0}",div);
+            Console.WriteLine("Mod of two number is :{0}",mod);
+            Console.ReadKey();
+        }
+    }
+}
